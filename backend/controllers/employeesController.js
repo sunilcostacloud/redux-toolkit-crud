ccc831bbe38a228aaad1244f825726a12f0444ee
sunilcostacloud <sunil@costacloud.com>
2024-01-03 15:04:01 +0530

    update

--- backend/controllers/employeesController.js
+++ backend/controllers/employeesController.js
@@ -2,106 +2,73 @@ const employees = require("../models/employeesSchema");
 
 // getEmployeesTable
 exports.getEmployeesTable = async (req, res) => {
-    const search = req.query.search || "";
-    const gender = req.query.gender || "";
-    const status = req.query.status || "";
-    const sort = req.query.sort || "";
-    const page = req.query.page || 1;
-    const ITEM_PER_PAGE = 2;
+  const search = req.query.search || "";
+  const gender = req.query.gender || "";
+  const status = req.query.status || "";
+  const sort = req.query.sort || "";
+  const page = req.query.page || 1;
+  const ITEM_PER_PAGE = 2;
 
-    const query = {
-        fname: { $regex: search, $options: "i" },
-    };
+  const query = {
+    fname: { $regex: search, $options: "i" },
+  };
 
-    if (gender !== "all") {
-        query.gender = gender;
-    }
+  if (gender !== "all") {
+    query.gender = gender;
+  }
 
-    if (status !== "all") {
-        query.status = status;
-    }
+  if (status !== "all") {
+    query.status = status;
+  }
 
-    try {
-        const skip = (page - 1) * ITEM_PER_PAGE; // 1 * 4 = 4
+  try {
+    const skip = (page - 1) * ITEM_PER_PAGE; // 1 * 4 = 4
 
-        const count = await employees.countDocuments(query);
+    const count = await employees.countDocuments(query);
 
-        const employeesTableData = await employees
-            .find(query)
-            .sort({ datecreated: sort == "new" ? -1 : 1 })
-            .limit(ITEM_PER_PAGE)
-            .skip(skip);
+    const employeesTableData = await employees
+      .find(query)
+      .sort({ datecreated: sort == "new" ? -1 : 1 })
+      .limit(ITEM_PER_PAGE)
+      .skip(skip);
 
-        const pageCount = Math.ceil(count / ITEM_PER_PAGE); // 8 /4 = 2
+    const pageCount = Math.ceil(count / ITEM_PER_PAGE); // 8 /4 = 2
 
-        res.status(200).json({
-            Pagination: {
-                count,
-                pageCount,
-            },
-            employeesTableData,
-        });
-    } catch (error) {
-        res.status(401).json(error);
-    }
-}
+    res.status(200).json({
+      Pagination: {
+        count,
+        pageCount,
+      },
+      employeesTableData,
+    });
+  } catch (error) {
+    res.status(401).json(error);
+  }
+};
 
 // addNewEmployee
 exports.addNewEmployee = async (req, res) => {
-    const { fname, lname, email, mobile, gender, location, status } = req.body;
-    if (
-        !fname ||
-        !lname ||
-        !email ||
-        !mobile ||
-        !gender ||
-        !location ||
-        !status
-    ) {
-        res.status(401).json("All Inputs is required");
-    }
-    try {
-        const preuser = await employees.findOne({ email: email });
+  const { fname, lname, email, mobile, gender, location, status } = req.body;
+  if (
+    !fname ||
+    !lname ||
+    !email ||
+    !mobile ||
+    !gender ||
+    !location ||
+    !status
+  ) {
+    res.status(401).json({ error: "All Inputs is required" });
+  }
+  try {
+    const preuser = await employees.findOne({ email: email });
 
-        if (preuser) {
-            res.status(401).json("This user already exist in our databse");
-        } else {
-            const datecreated = new Date();
+    if (preuser) {
+      res.status(401).json("This user already exist in our databse");
+    } else {
+      const datecreated = new Date();
 
-            const employeeData = new employees({
-                fname,
-                lname,
-                email,
-                mobile,
-                gender,
-                location,
-                status,
-                datecreated,
-            });
-            await employeeData.save();
-            res.status(201).json(employeeData);
-        }
-    } catch (error) {
-        res.status(401).json(error);
-        console.log("Adding employee catch block error", error);
-    }
-}
-
-// getSingleEmployeeDetails
-exports.getSingleEmployeeDetails = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const employeesdata = await employees.findOne({ _id: id });
-        res.status(200).json(employeesdata);
-    } catch (error) {
-        res.status(401).json(error);
-    }
-}
-
-// updateEmployeeDetails
-exports.updateEmployeeDetails = async (req, res) => {
-    const { id } = req.params;
-    const {
+      const employeeData = new employees({
         fname,
         lname,
         email,
@@ -109,42 +76,66 @@ exports.updateEmployeeDetails = async (req, res) => {
         gender,
         location,
         status,
-    } = req.body;
-
-    const dateUpdated = new Date();
-
-    try {
-        const updateEmployee = await employees.findByIdAndUpdate(
-            { _id: id },
-            {
-                fname,
-                lname,
-                email,
-                mobile,
-                gender,
-                location,
-                status,
-                dateUpdated,
-            },
-            {
-                new: true,
-            }
-        );
-
-        await updateEmployee.save();
-        res.status(200).json(updateEmployee);
-    } catch (error) {
-        res.status(401).json(error);
+        datecreated,
+      });
+      await employeeData.save();
+      res.status(201).json(employeeData);
     }
-}
+  } catch (error) {
+    res.status(401).json({ error: "cannot able to add employee" });
+  }
+};
+
+// getSingleEmployeeDetails
+exports.getSingleEmployeeDetails = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const employeesdata = await employees.findOne({ _id: id });
+    res.status(200).json(employeesdata);
+  } catch (error) {
+    res.status(401).json({ error: "user not found" });
+  }
+};
+
+// updateEmployeeDetails
+exports.updateEmployeeDetails = async (req, res) => {
+  const { id } = req.params;
+  const { fname, lname, email, mobile, gender, location, status } = req.body;
+
+  const dateUpdated = new Date();
+
+  try {
+    const updateEmployee = await employees.findByIdAndUpdate(
+      { _id: id },
+      {
+        fname,
+        lname,
+        email,
+        mobile,
+        gender,
+        location,
+        status,
+        dateUpdated,
+      },
+      {
+        new: true,
+      }
+    );
+
+    await updateEmployee.save();
+    res.status(200).json(updateEmployee);
+  } catch (error) {
+    res.status(401).json({ error: "cannot able to update employee" });
+  }
+};
 
 // deleteEmployee
 exports.deleteEmployee = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const deleteEmployee = await employees.findByIdAndDelete({ _id: id });
-        res.status(200).json(deleteEmployee);
-    } catch (error) {
-        res.status(401).json(error);
-    }
-}
+  const { id } = req.params;
+  try {
+    const deleteEmployee = await employees.findByIdAndDelete({ _id: id });
+    res.status(200).json(deleteEmployee);
+  } catch (error) {
+    res.status(401).json({ error: "cannot able to delete employee" });
+  }
+};
