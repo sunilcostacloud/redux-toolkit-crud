042d92223642420583270a66f11c31431fc09c02
sunilcostacloud <sunil@costacloud.com>
2024-01-05 10:22:14 +0530

    update

---
+++ frontend-nextjs/src/pages/_app.js
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+
+// ** Third Party Import
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
+}
